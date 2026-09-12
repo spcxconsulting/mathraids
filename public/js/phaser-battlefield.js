5db14244dc5d88a239bba548e7e2211c433c7416
spcxconsulting <ask@spcxconsulting.com>
2026-09-13 08:20:24 +0930

    perf: switch battlefield runtime to pixel canvas

--- public/js/phaser-battlefield.js
+++ public/js/phaser-battlefield.js
@@ -1,19 +1,3 @@
-import * as Phaser from '/vendor/phaser.esm.js';
-
-// Phaser 4 removed Phaser.Geom.Point. The lightweight battlefield still
-// passes point-like values into Graphics.fillPoints, so provide the tiny
-// compatibility shape here until the generated geometry is converted fully
-// to plain { x, y } objects.
-if (!Phaser.Geom.Point) {
-  Object.defineProperty(Phaser.Geom, 'Point', {
-    configurable: true,
-    value: class Point {
-      constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
-      }
-    }
-  });
-}
-
-export { createRaidBattlefield } from './phaser-battlefield-snappy.js';
+// Backwards-compatible module name while the rest of the client still imports
+// phaser-battlefield.js. The live renderer is now a tiny Canvas2D pixel engine.
+export { createRaidBattlefield } from './pixel-battlefield.js';
