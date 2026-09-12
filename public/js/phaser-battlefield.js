b890a988cb672f54ca924c241cdb153d0e5ed294
spcxconsulting <ask@spcxconsulting.com>
2026-09-13 08:04:13 +0930

    fix: restore Phaser 4 point compatibility

--- public/js/phaser-battlefield.js
+++ public/js/phaser-battlefield.js
@@ -1,1 +1,19 @@
+import * as Phaser from '/vendor/phaser.esm.js';
+
+// Phaser 4 removed Phaser.Geom.Point. The lightweight battlefield still
+// passes point-like values into Graphics.fillPoints, so provide the tiny
+// compatibility shape here until the generated geometry is converted fully
+// to plain { x, y } objects.
+if (!Phaser.Geom.Point) {
+  Object.defineProperty(Phaser.Geom, 'Point', {
+    configurable: true,
+    value: class Point {
+      constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+      }
+    }
+  });
+}
+
 export { createRaidBattlefield } from './phaser-battlefield-fast.js';
